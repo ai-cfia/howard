cb8227954909f0d1adeaa358fc453619aacb212c
ThomasCardin <tomcardin@outlook.com>
2024-01-05 08:39:31 -0500

    issue #1: removed unused cluster node pool

--- terraform/gcp-kubernetes-cluster/cluster.tf
+++ terraform/gcp-kubernetes-cluster/cluster.tf
@@ -68,47 +68,17 @@ resource "google_container_node_pool" "nodepool" {
     auto_upgrade = true
   }
 
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 10
+  }
+
   node_config {
     preemptible  = false
     machine_type = "e2-small"
 
     labels = {
       role = "general"
-    }
-
-    service_account = google_service_account.kubernetes.email
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
-  }
-}
-
-resource "google_container_node_pool" "spot" {
-  name    = "${var.cluster_name}-spot-nodepool"
-  cluster = google_container_cluster.cluster.id
-
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
-
-  autoscaling {
-    min_node_count = 0
-    max_node_count = 3
-  }
-
-  node_config {
-    preemptible  = true
-    machine_type = "e2-small"
-
-    labels = {
-      team = "devsecops"
-    }
-
-    taint {
-      key    = "instance_type"
-      value  = "${var.cluster_name}-spot"
-      effect = "NO_SCHEDULE"
     }
 
     service_account = google_service_account.kubernetes.email
