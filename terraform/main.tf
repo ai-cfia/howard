4a5b0b4e3d33076dbf21502fc0ae0c651513e3c9
SonOfLope <jonathanlopez@hotmail.ca>
2024-02-01 16:30:37 -0500

    Issue #3: testing terraform pipeline workflow

---
+++ terraform/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.0"
+    }
+    
+  }
+
+  backend "azurerm" {
+    resource_group_name   = "rg-ai-cfia-terraform-state"
+    storage_account_name  = "tfcfiastate"
+    container_name        = "infra-terraform-state"
+    key                   = "tf/terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "testResourceGroup-tf-ai-cfia"
+  location = var.region
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "region" {
+    description = "The Azure Regions in which the resources are located."
+    type        = string
+    default     = "Canada East"
+}
