abd27bbbdfaa7e89ee0005720889c6d7ed5cb18a
SonOfLope <jonathanlopez@hotmail.ca>
2024-02-01 16:44:20 -0500

    Issue #3: testing with region typo fix

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "region" {
     description = "The Azure Regions in which the resources are located."
     type        = string
-    default     = "Canada East"
+    default     = "canadaeast"
 }
