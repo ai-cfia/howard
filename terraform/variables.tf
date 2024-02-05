35f2ea58bf0ef2f1679a6a8b895874e9ee30cc2a
SonOfLope <jonathanlopez@hotmail.ca>
2024-02-05 13:12:30 -0500

    Issue #28: Change to reusable workflow for tf linting, add  tflint config file and add tf required version

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "region" {
-    description = "The Azure Regions in which the resources are located."
-    type        = string
-    default     = "canadaeast"
+  description = "The Azure Regions in which the resources are located."
+  type        = string
+  default     = "canadaeast"
 }
