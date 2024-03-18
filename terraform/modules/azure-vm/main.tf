f961fbf15a4be0232329dd39c80e90045819925c
ThomasCardin <tomcardin@outlook.com>
2024-03-18 09:29:08 -0400

    issue ##106: fix

--- terraform/modules/azure-vm/main.tf
+++ terraform/modules/azure-vm/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_virtual_machine" "vm" {
+resource "azurerm_virtual_machine" "linux-vm" {
   for_each = var.linux-vms
 
   name                  = each.value.name
@@ -35,4 +35,24 @@ resource "azurerm_virtual_machine" "vm" {
   }
 
   tags = each.value.tags
+}
+
+resource "azurerm_public_ip" "bastion_public_ip" {
+  name                = var.bastion_public_ip_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = var.bastion_public_ip_allocation_method
+  sku                 = var.bastion_public_ip_sku
+}
+
+resource "azurerm_bastion_host" "bastion_host" {
+  name                = var.bastion_host_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                 = var.bastion_host_ip_configuration_name
+    subnet_id            = azurerm_subnet.vm_virtual_network_subnet.id
+    public_ip_address_id = azurerm_public_ip.bastion_public_ip.id
+  }
 }
