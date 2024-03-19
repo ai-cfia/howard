62229ffcf7bf2336c5297d902eb058bba97d5703
Thomas Cardin <49320132+ThomasCardin@users.noreply.github.com>
2024-03-19 10:11:09 -0400

    issue #106: azure vm terraform module (#107)
    * issue #106: azure vm terraform module
    
    * issue #106: added provider inside azure-vm module
    
    * issue #106: changed vms to linux-vms
    
    * issue ##106: fix
    
    * issue #106: added windows vm configuration

---
+++ terraform/modules/azure-vm/main.tf
@@ -0,0 +1,75 @@
+resource "azurerm_linux_virtual_machine" "linux-vm" {
+  for_each = var.linux-vms
+
+  name                  = each.value.name
+  size                  = each.value.size
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.vm_network_interface.id]
+
+  computer_name  = each.value.name
+  admin_username = each.value.admin_username
+  admin_password = each.value.admin_password
+
+  source_image_reference {
+    publisher = each.value.storage_image_reference.publisher
+    offer     = each.value.storage_image_reference.offer
+    sku       = each.value.storage_image_reference.sku
+    version   = each.value.storage_image_reference.version
+  }
+
+  os_disk {
+    caching              = each.value.storage_os_disk.caching
+    storage_account_type = each.value.storage_os_disk.managed_disk_type
+  }
+
+  tags = each.value.tags
+}
+
+resource "azurerm_windows_virtual_machine" "windows-vm" {
+  for_each = var.windows-vms
+
+  name                  = each.value.name
+  size                  = each.value.size
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.vm_network_interface.id]
+
+  computer_name  = each.value.name
+  admin_username = each.value.admin_username
+  admin_password = each.value.admin_password
+
+  source_image_reference {
+    publisher = each.value.storage_image_reference.publisher
+    offer     = each.value.storage_image_reference.offer
+    sku       = each.value.storage_image_reference.sku
+    version   = each.value.storage_image_reference.version
+  }
+
+  os_disk {
+    caching              = each.value.storage_os_disk.caching
+    storage_account_type = each.value.storage_os_disk.managed_disk_type
+  }
+
+  tags = each.value.tags
+}
+
+resource "azurerm_public_ip" "bastion_public_ip" {
+  name                = var.bastion_public_ip_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = var.bastion_public_ip_allocation_method
+  sku                 = var.bastion_public_ip_sku
+}
+
+resource "azurerm_bastion_host" "bastion_host" {
+  name                = var.bastion_host_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                 = var.bastion_host_ip_configuration_name
+    subnet_id            = azurerm_subnet.vm_virtual_network_subnet.id
+    public_ip_address_id = azurerm_public_ip.bastion_public_ip.id
+  }
+}
---
+++ terraform/modules/azure-vm/variables.tf
@@ -0,0 +1,114 @@
+variable "vm_rg_name" {
+  type        = string
+  description = "The resource group name for each vm's"
+}
+
+variable "vm_location" {
+  type        = string
+  description = "Location of each vm's"
+}
+
+variable "vm_virtual_network_name" {
+  type        = string
+  description = "VM virtual network name"
+}
+
+variable "vm_virtual_network_address_space" {
+  type        = list(string)
+  description = "VM virtual network name address space"
+}
+
+variable "vm_virtual_network_subnet_name" {
+  type        = string
+  description = "VM virtual network subnet name"
+}
+
+variable "vm_virtual_network_subnet_address_prefixes" {
+  type        = list(string)
+  description = "VM virtual network subnet address prefixed"
+}
+
+variable "vm_network_interface_name" {
+  type        = string
+  description = "VM virtual network interface name"
+}
+
+variable "vm_network_interface_ip_configuration_name" {
+  type        = string
+  description = "VM virtual network ip configuration name"
+}
+
+variable "vm_network_interface_ip_configuration_type" {
+  type        = string
+  description = "VM network interface ip configuration type"
+  default     = "Dynamic"
+}
+
+variable "linux-vms" {
+  type = map(object({
+    name           = string
+    size           = string
+    tags           = map(string)
+    admin_username = string
+    admin_password = string
+    source_image_reference = object({
+      publisher = string
+      offer     = string
+      sku       = string
+      version   = string
+    })
+    os_disk = object({
+      caching              = string
+      storage_account_type = string
+    })
+  }))
+  description = "Linux VM configuration"
+}
+
+variable "windows-vms" {
+  type = map(object({
+    name           = string
+    size           = string
+    tags           = map(string)
+    admin_username = string
+    admin_password = string
+    source_image_reference = object({
+      publisher = string
+      offer     = string
+      sku       = string
+      version   = string
+    })
+    os_disk = object({
+      caching              = string
+      storage_account_type = string
+    })
+  }))
+  description = "Windows VM configuration"
+}
+
+variable "bastion_public_ip_name" {
+  type        = string
+  description = "Bastion public IP name"
+}
+
+variable "bastion_public_ip_allocation_method" {
+  type        = string
+  description = "Bastion public IP name"
+  default     = "Static"
+}
+
+variable "bastion_public_ip_sku" {
+  type        = string
+  description = "Bastion public IP sku"
+  default     = "Standard"
+}
+
+variable "bastion_host_name" {
+  type        = string
+  description = "Bastion hostsname"
+}
+
+variable "bastion_host_ip_configuration_name" {
+  type        = string
+  description = "Bastion host IP configuration name"
+}
