e6b502bc83152d38a0cd19c43d61fe6386dffe9c
Thomas Cardin <49320132+ThomasCardin@users.noreply.github.com>
2024-04-23 13:01:20 -0400

    issue #205: typo

--- terraform/modules/azure-kubernetes-gpu-cluster/output.tf
+++ terraform/modules/azure-kubernetes-gpu-cluster/output.tf
@@ -1,21 +1,21 @@
 output "cluster_name" {
   description = "Cluster name to be used in the context of kubectl"
-  value       = azurerm_kubernetes_cluster.k8s.name
+  value       = azurerm_kubernetes_cluster.k8s-gpu.name
 }
 
 output "cluster_ca_certificate" {
   description = "Cluster certificate"
-  value       = azurerm_kubernetes_cluster.k8s.kube_config[0].cluster_ca_certificate
+  value       = azurerm_kubernetes_cluster.k8s-gpu.kube_config[0].cluster_ca_certificate
 }
 
 output "cluster_principal_id" {
   description = "AKS cluster principal id"
-  value       = azurerm_kubernetes_cluster.k8s.identity[0].principal_id
+  value       = azurerm_kubernetes_cluster.k8s-gpu.identity[0].principal_id
 }
 
 output "kv_identity_resource_id" {
   description = "The client ID of the managed identity used by the Azure Key Vault Secrets Provider"
-  value       = azurerm_kubernetes_cluster.k8s.key_vault_secrets_provider[0].secret_identity[0].object_id
+  value       = azurerm_kubernetes_cluster.k8s-gpu.key_vault_secrets_provider[0].secret_identity[0].object_id
 }
 
 #output "cluster_kubelet_identity" {
