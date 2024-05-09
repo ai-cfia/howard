9cce7760d8bbf6801d3dd2fd027642eaa0c03734
SonOfLope <jonathanlopez@hotmail.ca>
2024-05-09 14:24:30 -0400

    Issue #222: convert value to array for additional subnet

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -175,7 +175,7 @@ resource "azurerm_subnet" "subnet_dev" {
   name                 = "subnet-dev"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = module.cluster-network-0.virtual_network_name
-  address_prefixes     = var.dev_subnet_address
+  address_prefixes     = [var.dev_subnet_address]
 }
 
 # module "vms" {
