06181649f81801f090470754adc720d99cb0835e
Thomas Cardin <49320132+ThomasCardin@users.noreply.github.com>
2024-06-20 15:45:04 -0400

    issue #284: removed unused tf providers

--- terraform/staging/providers.tf
+++ terraform/staging/providers.tf
@@ -3,10 +3,6 @@ terraform {
   required_version = ">= 1.7.2"
 
   required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.25"
@@ -15,10 +11,6 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "2.30.0"
     }
-    # aws = {
-    #   source  = "hashicorp/aws"
-    #   version = "~> 3.0"
-    # }
     azuread = {
       source  = "hashicorp/azuread"
       version = "2.47.0"
