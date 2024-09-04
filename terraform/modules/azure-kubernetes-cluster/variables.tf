28f7d20927137cc729b14228d5e3b56e4c9bdf1f
ThomasCardin <tomcardin@outlook.com>
2024-09-04 10:54:19 -0400

    issue #348: aks backup configuration

--- terraform/modules/azure-kubernetes-cluster/variables.tf
+++ terraform/modules/azure-kubernetes-cluster/variables.tf
@@ -176,6 +176,62 @@ variable "user_assigned_identity_ids" {
   default     = []
 }
 
+variable "azure_backup_vault_name" {
+  description = "Azure Backup Vault name"
+  type        = string
+}
+
+variable "azure_backup_datastore_type" {
+  description = "Azure Backup Datastore type"
+  type        = string
+}
+
+variable "azure_backup_redundancy" {
+  description = "Azure Backup Redundancy"
+  type        = string
+}
+
+variable "azure_backuppolicy_name" {
+  description = "Azure Backup Policy name"
+  type        = string
+}
+
+variable "azure_backup_extension_type" {
+  description = "Type of the AKS Cluster Extension."
+  type        = string
+  default     = "microsoft.dataprotection.kubernetes"
+}
+
+variable "backup_excluded_namespaces" {
+  description = "K8s Namespaces to exclude from backup."
+  type        = list(string)
+  default     = []
+}
+
+variable "backup_excluded_resource_types" {
+  description = "K8s Resource Types to exclude from backup."
+  type        = list(string)
+  default     = []
+}
+
+variable "backup_included_namespaces" {
+  description = "K8s Namespaces to include in backup."
+  type        = list(string)
+  default     = []
+}
+
+variable "backup_included_resource_types" {
+  description = "K8s Resource Types to include in backup."
+  type        = list(string)
+  default     = []
+}
+
+variable "backup_label_selectors" {
+  description = "K8s Label Selectors to include in backup."
+  type        = list(string)
+  default     = []
+}
+
 # variable "aks_service_mesh_profile" {
 #   description = "AKS Service mesh profile"
 #   default     = "Istio"
