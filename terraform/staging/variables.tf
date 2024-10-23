936ff4f8a0fd2995563c56212e8c6f0066415d4c
ThomasCardin <tomcardin@outlook.com>
2024-10-23 14:37:50 -0400

    issue #405: terraform fmt

--- terraform/staging/variables.tf
+++ terraform/staging/variables.tf
@@ -256,42 +256,12 @@ variable "postgresql_sku_name" {
   type = string
 }
 
-variable "postgresql_storage_mb" {
-  type    = number
-  default = 640000
-}
-
-variable "postgresql_backup_auto_grow" {
-  type    = bool
-  default = false
-}
-
-variable "postgresql_backup_retention_days" {
-  type    = number
-  default = 7
-}
-
-variable "postgresql_backup_geo_redundant" {
-  type    = bool
-  default = false
-}
-
 variable "postgresql_backup_admin_login" {
   type = string
 }
 
 variable "postgresql_backup_admin_password" {
   type = string
-}
-
-variable "postgresql_backup_version" {
-  type    = string
-  default = "11"
-}
-
-variable "postgresql_backup_ssl_enforcement_enabled" {
-  type    = bool
-  default = true
 }
 
 # variable "vm_rg_name" {
