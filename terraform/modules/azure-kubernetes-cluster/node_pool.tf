e61d35e8d895f448a6f7fde48d92849346adf9fa
ThomasCardin <tomcardin@outlook.com>
2024-10-30 15:53:00 -0400

    terraform azurerm upgrade

--- terraform/modules/azure-kubernetes-cluster/node_pool.tf
+++ terraform/modules/azure-kubernetes-cluster/node_pool.tf
@@ -7,7 +7,7 @@ resource "azurerm_kubernetes_cluster_node_pool" "aks" {
   name                  = substr(each.key, 0, 12)
   vm_size               = each.value.vm_size
   os_disk_size_gb       = each.value.os_disk_size_gb
-  enable_auto_scaling   = each.value.enable_auto_scaling
+  auto_scaling_enabled  = each.value.enable_auto_scaling
   zones                 = each.value.zones
   node_count            = each.value.node_count
   min_count             = each.value.min_count
