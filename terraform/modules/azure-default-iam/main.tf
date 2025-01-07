3ca6a71dda01304b0f0835f23262e4b1277520a4
Thomas Cardin <49320132+ThomasCardin@users.noreply.github.com>
2025-01-07 10:32:33 -0500

    issue #510: rework data

--- terraform/modules/azure-default-iam/main.tf
+++ terraform/modules/azure-default-iam/main.tf
@@ -1,3 +1,8 @@
+data "azuread_user" "users" {
+  for_each            = { for email in var.var.user_emails : email => email }
+  user_principal_name = each.value
+}
+
 resource "azurerm_role_definition" "custom_role" {
   name        = "tf_default_ai_cfia_az_role"
   scope       = "/subscriptions/${var.subscription_id}"
@@ -14,13 +19,8 @@ resource "azurerm_role_definition" "custom_role" {
 }
 
 resource "azurerm_role_assignment" "role_assignments" {
-  for_each             = toset(var.user_emails)
+  for_each             = data.azuread_user.users
   scope                = "/subscriptions/${var.subscription_id}"
   role_definition_name = azurerm_role_definition.custom_role.name
-  principal_id         = data.azuread_user["user_${each.key}"].id
-}
-
-data "azuread_user" "users" {
-  for_each            = toset(var.user_emails)
-  user_principal_name = each.key
+  principal_id         = each.value.id
 }
