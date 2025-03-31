7b9146fa86b9a5609f7d9b24c2a0178629323833
ThomasCardin <tomcardin@outlook.com>
2025-03-31 09:18:31 -0400

    cluster, vault shutdown

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -14,18 +14,18 @@ provider "azurerm" {
 
 provider "azuread" {}
 
-module "cluster-network-0" {
-  source              = "../modules/azure-cluster-network"
-  location            = var.location_1
-  resource_group_name = azurerm_resource_group.rg.name
+# module "cluster-network-0" {
+#   source              = "../modules/azure-cluster-network"
+#   location            = var.location_1
+#   resource_group_name = azurerm_resource_group.rg.name
 
-  vnet_name   = "vnet-${var.aks_name}"
-  subnet_name = "subnet-${var.aks_name}"
+#   vnet_name   = "vnet-${var.aks_name}"
+#   subnet_name = "subnet-${var.aks_name}"
 
-  address_space           = [var.virtual_network_address]
-  subnet_address_prefixes = [var.subnet_address]
-  tags                    = var.tags
-}
+#   address_space           = [var.virtual_network_address]
+#   subnet_address_prefixes = [var.subnet_address]
+#   tags                    = var.tags
+# }
 
 module "azure-dns-staging" {
   source = "../modules/azure-dns"
@@ -42,76 +42,76 @@ module "azure-dns-staging" {
   soa_record_tech_contact_email = var.soa_record_tech_contact_email
 }
 
-module "aks-cluster-0" {
+# module "aks-cluster-0" {
 
-  depends_on = [module.cluster-network-0]
+#   depends_on = [module.cluster-network-0]
 
-  source = "../modules/azure-kubernetes-cluster"
+#   source = "../modules/azure-kubernetes-cluster"
 
-  prefix         = var.aks_name
-  resource_group = azurerm_resource_group.rg.name
-  location       = azurerm_resource_group.rg.location
+#   prefix         = var.aks_name
+#   resource_group = azurerm_resource_group.rg.name
+#   location       = azurerm_resource_group.rg.location
 
-  k8s_version = var.k8s_version
+#   k8s_version = var.k8s_version
 
-  auto_scaling_default_node = var.auto_scaling_default_node
-  zones                     = var.zones
-  vm_size                   = var.default_node_vm_size
-  max_pods                  = var.max_pods
-  node_count                = var.node_count
-  node_min_count            = var.node_min_count
-  node_max_count            = var.node_max_count
+#   auto_scaling_default_node = var.auto_scaling_default_node
+#   zones                     = var.zones
+#   vm_size                   = var.default_node_vm_size
+#   max_pods                  = var.max_pods
+#   node_count                = var.node_count
+#   node_min_count            = var.node_min_count
+#   node_max_count            = var.node_max_count
 
-  rbac_enabled               = var.rbac_enabled
-  aks_admin_group_object_ids = var.aks_admin_group_object_ids
-  # ad_groups                  = var.ad_groups
-  # ad_members                 = var.ad_members
+#   rbac_enabled               = var.rbac_enabled
+#   aks_admin_group_object_ids = var.aks_admin_group_object_ids
+#   # ad_groups                  = var.ad_groups
+#   # ad_members                 = var.ad_members
 
-  network_resource_group = module.cluster-network-0.resource_group_name
-  network_vnet           = module.cluster-network-0.virtual_network_name
-  network_subnet         = module.cluster-network-0.subnet_name
+#   network_resource_group = module.cluster-network-0.resource_group_name
+#   network_vnet           = module.cluster-network-0.virtual_network_name
+#   network_subnet         = module.cluster-network-0.subnet_name
 
-  service_cidr         = var.service_cidr
-  dns_service_ip       = var.dns_service_ip
-  pod_cidr             = var.pod_cidr
-  storage_account_name = null
+#   service_cidr         = var.service_cidr
+#   dns_service_ip       = var.dns_service_ip
+#   pod_cidr             = var.pod_cidr
+#   storage_account_name = null
 
-  additional_node_pools = var.additional_node_pools
+#   additional_node_pools = var.additional_node_pools
 
-  tags = var.tags
+#   tags = var.tags
 
-  sku_tier = var.sku_tier
+#   sku_tier = var.sku_tier
 
-  # Backup's configuration
-  azure_backup_vault_name             = var.azure_backup_vault_name
-  azure_backup_datastore_type         = var.azure_backup_datastore_type
-  azure_backup_redundancy             = var.azure_backup_redundancy
-  azure_backuppolicy_name             = var.azure_backuppolicy_name
-  azure_backup_storage_account_name   = var.azure_backup_storage_account_name
-  azure_backup_storage_container_name = var.azure_backup_storage_container_name
-  azure_backup_trusted_access         = var.azure_backup_trusted_access
+#   # Backup's configuration
+#   azure_backup_vault_name             = var.azure_backup_vault_name
+#   azure_backup_datastore_type         = var.azure_backup_datastore_type
+#   azure_backup_redundancy             = var.azure_backup_redundancy
+#   azure_backuppolicy_name             = var.azure_backuppolicy_name
+#   azure_backup_storage_account_name   = var.azure_backup_storage_account_name
+#   azure_backup_storage_container_name = var.azure_backup_storage_container_name
+#   azure_backup_trusted_access         = var.azure_backup_trusted_access
 
-  backup_excluded_namespaces     = var.backup_excluded_namespaces
-  backup_excluded_resource_types = var.backup_excluded_resource_types
-  backup_included_namespaces     = var.backup_included_namespaces
-  backup_included_resource_types = var.backup_included_resource_types
-  backup_label_selectors         = var.backup_label_selectors
-}
+#   backup_excluded_namespaces     = var.backup_excluded_namespaces
+#   backup_excluded_resource_types = var.backup_excluded_resource_types
+#   backup_included_namespaces     = var.backup_included_namespaces
+#   backup_included_resource_types = var.backup_included_resource_types
+#   backup_label_selectors         = var.backup_label_selectors
+# }
 
-module "vault" {
-  source         = "../modules/vault"
-  location       = var.location_1
-  resource_group = azurerm_resource_group.rg.name
-  prefix         = var.environment
+# module "vault" {
+#   source         = "../modules/vault"
+#   location       = var.location_1
+#   resource_group = azurerm_resource_group.rg.name
+#   prefix         = var.environment
 
-  cluster_principal_id    = module.aks-cluster-0.cluster_principal_id
-  ca_cluster              = module.aks-cluster-0.cluster_ca_certificate
-  kv_identity_resource_id = module.aks-cluster-0.kv_identity_resource_id
+#   cluster_principal_id    = module.aks-cluster-0.cluster_principal_id
+#   ca_cluster              = module.aks-cluster-0.cluster_ca_certificate
+#   kv_identity_resource_id = module.aks-cluster-0.kv_identity_resource_id
 
-  providers = {
-    kubernetes = kubernetes
-  }
-}
+#   providers = {
+#     kubernetes = kubernetes
+#   }
+# }
 
 # module "fertiscan-postgresql-server" {
 #   source                 = "../modules/azure-postgresql-flexible-server"
@@ -148,12 +148,12 @@ module "vault" {
 # }
 
 # Subnet dedicated to provide internal access From Finesse to protected services from Dev
-resource "azurerm_subnet" "subnet_dev" {
-  name                 = "subnet-dev"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = module.cluster-network-0.virtual_network_name
-  address_prefixes     = [var.dev_subnet_address]
-}
+# resource "azurerm_subnet" "subnet_dev" {
+#   name                 = "subnet-dev"
+#   resource_group_name  = azurerm_resource_group.rg.name
+#   virtual_network_name = module.cluster-network-0.virtual_network_name
+#   address_prefixes     = [var.dev_subnet_address]
+# }
 
 # module "vms" {
 #  source = "../modules/azure-vm"
